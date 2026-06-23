9f457126265d25bbe6ef7a3b5c81d33ec2d3cc39
Alexander Brummer <alexander.brummer@cariad.technology>
2026-06-23 16:50:53 +0200

    Finish assignment 6 part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -10,15 +10,60 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <pthread.h>
+#include <time.h>
+#include "queue.h"
 
+struct thread_data {
+    pthread_t tid;
+    int conn_fd;
+    int completed;
+};
+
+struct entry {
+    struct thread_data* tdata;
+    SLIST_ENTRY(entry) entries;
+};
+
+SLIST_HEAD(slisthead, entry);
+
+FILE* file = NULL;
+pthread_mutex_t file_mutex;
 int terminate = 0;
 
 static void signal_handler(int signal_number) {
-    syslog(LOG_DEBUG, "Caught signal, exiting\n");
-    terminate = 1;
+    if (signal_number == SIGINT || signal_number == SIGTERM) {
+        syslog(LOG_DEBUG, "Caught signal, exiting\n");
+        terminate = 1;
+    } else if (signal_number == SIGALRM) {
+        char timestr[50];        
+
+        time_t t = time(NULL);
+        struct tm *localt = localtime(&t);
+        if (localt == NULL) {
+            syslog(LOG_ERR, "Error in localtime(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        strftime(timestr, sizeof(timestr), "timestamp: %a, %d %b %Y %T %z", localt);
+
+        if (pthread_mutex_lock(&file_mutex) != 0) {
+            syslog(LOG_ERR, "Error in pthread_mutex_lock(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        if (fprintf(file, "%s\n", timestr) < 0) {
+            syslog(LOG_ERR, "Error in fprintf(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        if (pthread_mutex_unlock(&file_mutex) != 0) {
+            syslog(LOG_ERR, "Error in pthread_mutex_unlock(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        alarm(10);
+    }
 }
 
-void send_file_back(FILE* file, int conn_fd) {
+void send_file_back(int conn_fd) {
     if (fseek(file, 0, SEEK_SET) == -1) {
         syslog(LOG_ERR, "Error in fseek(): %s\n", strerror(errno));
         exit(EXIT_FAILURE);
@@ -38,17 +83,93 @@ void send_file_back(FILE* file, int conn_fd) {
     }
 }
 
-void listen_and_reply(int sock_fd) {
+void* handleConnection(void* arg) {
+    struct thread_data* tdata = (struct thread_data*) arg;
+    int conn_fd = tdata->conn_fd;
+    int max_bytes = 100;
+    char* buf = malloc(max_bytes);
+    if (buf == NULL) {
+        syslog(LOG_ERR, "Error in malloc(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    int num_bytes;
+    int bytes_sofar = 0;
+    while ((num_bytes = recv(conn_fd, buf + bytes_sofar, max_bytes - 1, 0)) > 0
+            || (num_bytes < 0 && !terminate && (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR))) {
+        if (num_bytes < 0 && (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)) {
+            continue;
+        }
+        bytes_sofar += num_bytes;
+        if (buf[bytes_sofar - 1] == '\n') {
+            buf[bytes_sofar] = '\0';
+            if (pthread_mutex_lock(&file_mutex) != 0) {
+                syslog(LOG_ERR, "Error in pthread_mutex_lock(): %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+            if (fprintf(file, "%s", buf) < 0) {
+                syslog(LOG_ERR, "Error in fprintf(): %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+            send_file_back(conn_fd);
+            if (pthread_mutex_unlock(&file_mutex) != 0) {
+                syslog(LOG_ERR, "Error in pthread_mutex_unlock(): %s\n", strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+            bytes_sofar = 0;
+        }
+        buf = realloc(buf, bytes_sofar + max_bytes);
+        if (buf == NULL) {
+            syslog(LOG_ERR, "Error in realloc(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+    free(buf);
+    if (num_bytes == -1 && !terminate) {
+        syslog(LOG_ERR, "Error in recv(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_mutex_lock(&file_mutex) != 0) {
+        syslog(LOG_ERR, "Error in pthread_mutex_lock(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    if (fflush(file) == EOF) {
+        syslog(LOG_ERR, "Error in fflush(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    if (pthread_mutex_unlock(&file_mutex) != 0) {
+        syslog(LOG_ERR, "Error in pthread_mutex_unlock(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    close(conn_fd);
+
+    tdata->completed = 1;
+    return NULL;
+}
+
+void listen_and_accept(int sock_fd) {
     if (listen(sock_fd, 10) != 0) {
         syslog(LOG_ERR, "Error in listen(): %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    FILE* f = fopen("/var/tmp/aesdsocketdata", "a+");
-    if (f == NULL) {
+    file = fopen("/var/tmp/aesdsocketdata", "a+");
+    if (file == NULL) {
         syslog(LOG_ERR, "Could not open file %s: %s\n", "/var/tmp/aesdsocketdata", strerror(errno));
         exit(EXIT_FAILURE);
     }
+
+    if (pthread_mutex_init(&file_mutex, NULL) != 0) {
+        syslog(LOG_ERR, "Error in pthread_mutex_init(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    alarm(10);
+
+    struct slisthead head;
+    SLIST_INIT(&head);
+    struct entry* e;
+    struct entry* te;
     
     struct sockaddr_in conn_addr;
     socklen_t addr_len = sizeof(conn_addr);
@@ -60,10 +181,17 @@ void listen_and_reply(int sock_fd) {
         if (conn_fd == -1) {
             if (terminate) {
                 break;
+            } else if (errno == EINTR) {
+                continue;
             }
             syslog(LOG_ERR, "Error in accept(): %s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
+
+        struct timeval tv;
+        tv.tv_sec = 2;
+        tv.tv_usec = 0;
+        setsockopt(conn_fd, SOL_SOCKET, SO_RCVTIMEO, (const char*) &tv, sizeof tv);
 
         struct in_addr* ip_addr = &conn_addr.sin_addr;
         char ip_str[INET_ADDRSTRLEN];
@@ -74,53 +202,59 @@ void listen_and_reply(int sock_fd) {
 
         syslog(LOG_DEBUG, "Accepted connection from %s\n", ip_str);
 
-        int max_bytes = 100;
-        char* buf = malloc(max_bytes);
-        if (buf == NULL) {
+        struct thread_data* new_tdata = (struct thread_data*) malloc(sizeof(struct thread_data));
+        if (new_tdata == NULL) {
             syslog(LOG_ERR, "Error in malloc(): %s\n", strerror(errno));
             exit(EXIT_FAILURE);
         }
-        int num_bytes;
-        int bytes_sofar = 0;
-        while ((num_bytes = recv(conn_fd, buf + bytes_sofar, max_bytes - 1, 0)) > 0) {
-            bytes_sofar += num_bytes;
-            if (buf[bytes_sofar - 1] == '\n') {
-                buf[bytes_sofar] = '\0';
-                if (fprintf(f, "%s", buf) < 0) {
-                    syslog(LOG_ERR, "Error in fprintf(): %s\n", strerror(errno));
+        new_tdata->conn_fd = conn_fd;
+        new_tdata->completed = 0;
+        struct entry* new_entry = (struct entry*) malloc(sizeof(struct entry));
+        if (new_entry == NULL) {
+            syslog(LOG_ERR, "Error in malloc(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        new_entry->tdata = new_tdata;
+
+        if (pthread_create(&new_tdata->tid, NULL, handleConnection, (void*) new_tdata) != 0) {
+            syslog(LOG_ERR, "Error in pthread_create(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        SLIST_INSERT_HEAD(&head, new_entry, entries);
+        
+        SLIST_FOREACH_SAFE(e, &head, entries, te) {
+            if (e->tdata->completed) {
+                if (pthread_join(e->tdata->tid, NULL) != 0) {
+                    syslog(LOG_ERR, "Error in pthread_join(): %s\n", strerror(errno));
                     exit(EXIT_FAILURE);
                 }
-                send_file_back(f, conn_fd);
-                bytes_sofar = 0;
-            }
-            buf = realloc(buf, bytes_sofar + max_bytes);
-            if (buf == NULL) {
-                syslog(LOG_ERR, "Error in realloc(): %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
+                SLIST_REMOVE(&head, e, entry, entries);
+                free(e->tdata);
+                free(e);
             }
         }
-        free(buf);
-        if (num_bytes == -1) {
-            if (terminate) {
-                close(conn_fd);
-                break;
-            }
-            syslog(LOG_ERR, "Error in recv(): %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-
-        if (fflush(f) == EOF) {
-            syslog(LOG_ERR, "Error in fflush(): %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
-        close(conn_fd);
 
         if (terminate) {
             break;
         }
     }
 
-    fclose(f);
+    SLIST_FOREACH_SAFE(e, &head, entries, te) {
+        if (pthread_join(e->tdata->tid, NULL) != 0) {
+            syslog(LOG_ERR, "Error in pthread_join(): %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        SLIST_REMOVE(&head, e, entry, entries);
+        free(e->tdata);
+        free(e);
+    }
+
+    if (pthread_mutex_destroy(&file_mutex) != 0) {
+        syslog(LOG_ERR, "Error in pthread_mutex_destroy(): %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(file);
     if (remove("/var/tmp/aesdsocketdata") == -1) {
         syslog(LOG_ERR, "Error in remove(): %s\n", strerror(errno));
         exit(EXIT_FAILURE);
@@ -134,7 +268,7 @@ int main(int argc, char** args) {
     struct sigaction action;
     memset(&action, 0, sizeof(struct sigaction));
     action.sa_handler = signal_handler;
-    if (sigaction(SIGTERM, &action, NULL) != 0 || sigaction(SIGINT, &action, NULL)) {
+    if (sigaction(SIGTERM, &action, NULL) != 0 || sigaction(SIGINT, &action, NULL) != 0 || sigaction(SIGALRM, &action, NULL) != 0) {
         syslog(LOG_ERR, "Error in sigaction(): %s\n", strerror(errno));
         return EXIT_FAILURE;
     }
@@ -150,23 +284,27 @@ int main(int argc, char** args) {
     int ret = getaddrinfo(NULL, "9000", &hints, &res);
     if (ret != 0) {
         syslog(LOG_ERR, "Error in inet_ntop(): %s\n", gai_strerror(ret));
+        freeaddrinfo(res);
         return EXIT_FAILURE;
     }
 
     int sock_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sock_fd == -1) {
         syslog(LOG_ERR, "Error in socket(): %s\n", strerror(errno));
+        freeaddrinfo(res);
         return EXIT_FAILURE;
     }
 
     int opt = 1;
     if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) != 0) {
         syslog(LOG_ERR, "Error in setsockopt(): %s\n", strerror(errno));
+        freeaddrinfo(res);
         return EXIT_FAILURE;
     }
 
     if (bind(sock_fd, res->ai_addr, res->ai_addrlen) != 0) {
         syslog(LOG_ERR, "Error in bind(): %s\n", strerror(errno));
+        freeaddrinfo(res);
         return EXIT_FAILURE;
     }
 
@@ -186,10 +324,10 @@ int main(int argc, char** args) {
             open("/dev/null", O_RDONLY);
             open("/dev/null", O_WRONLY);
             open("/dev/null", O_WRONLY);
-            listen_and_reply(sock_fd);
+            listen_and_accept(sock_fd);
         }
     } else {
-        listen_and_reply(sock_fd);
+        listen_and_accept(sock_fd);
     }
     
     closelog();
